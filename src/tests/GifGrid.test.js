c5b54496c59587ce09928b4676daf8d81a6cb83c
elimuri <elimurillo95@gmail.com>
2021-09-25 18:13:42 +0200

    añade test mockeando gifs

--- src/tests/GifGrid.test.js
+++ src/tests/GifGrid.test.js
@@ -1,11 +1,43 @@
 import { shallow } from "enzyme"
 import React from 'react'
 import { GifGrid } from "../components/GifGrid";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
+jest.mock("../hooks/useFetchGifs");
 
 describe('Pruebas en <AddCategory />', () => {
-    let wrapper = shallow(<GifGrid category={'Hollywood monsters'} />);
-
     test('debe mostrarse correctamente', () => {
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true
+        })
+
+        const wrapper = shallow(<GifGrid category={"Hollywood monsters"} />);
         expect(wrapper).toMatchSnapshot();
+    })
+
+    test('debe mostrar items cuando se cargan imagenes useFetchGifs', () => {
+        const gifs = [
+          {
+            id: "ABC",
+            url: "https://localhost/cualquier/cosa.jpg",
+            title: "Cualquier cosa",
+          },
+          {
+            id: "123",
+            url: "https://localhost/cualquier/cosa.jpg",
+            title: "Cualquier cosa",
+          }
+        ];
+
+        useFetchGifs.mockReturnValue({
+          data: gifs,
+          loading: false,
+        });
+
+        const wrapper = shallow(<GifGrid category={"Hollywood monsters"} />);
+        
+        expect(wrapper.find('p').exists()).toBe(false);
+        expect(wrapper.find('GifGridItem').length).toBe(gifs.length);
     })
 });
